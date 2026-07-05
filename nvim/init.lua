695bde7dc283207119e0a68ba7c599579bae7317
Aiaj Rijvee <rijveeaiaj@gmail.com>
2026-07-05 17:33:32 -0400

    Fix deprecated lspconfig API, use vim.lsp.config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,8 @@
 -- ~/.config/nvim/init.lua
 
 -- Basic sensible defaults
-vim.opt.number = true          -- show line numbers
-vim.opt.relativenumber = true  -- relative numbers (great for jumps like 5j, 3k)
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
@@ -13,12 +13,11 @@ vim.opt.smartcase = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
-vim.opt.clipboard = "unnamedplus" -- share clipboard with system
+vim.opt.clipboard = "unnamedplus"
 
--- Leader key (used for custom shortcuts later)
 vim.g.mapleader = " "
 
--- Bootstrap lazy.nvim (plugin manager)
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -32,7 +31,116 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Plugin list (empty for now, we'll add here next)
 require("lazy").setup({
-  -- plugins go here
+
+  ------------------------------------------------------------------
+  -- COLORSCHEME
+  ------------------------------------------------------------------
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd("colorscheme tokyonight-storm") -- matches your WezTerm theme
+    end,
+  },
+
+  ------------------------------------------------------------------
+  -- FUZZY FINDER (Telescope)
+  ------------------------------------------------------------------
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+    end,
+  },
+
+  ------------------------------------------------------------------
+  -- FILE EXPLORER (nvim-tree)
+  ------------------------------------------------------------------
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("nvim-tree").setup({})
+      vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    end,
+  },
+
+  ------------------------------------------------------------------
+  -- LSP + AUTOCOMPLETE
+  ------------------------------------------------------------------
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "pyright", "ts_ls" }, -- adjust to languages you use
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "hrsh7th/cmp-nvim-lsp" },
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- New Neovim 0.11+ API: configure servers via vim.lsp.config, then enable them.
+      -- (Replaces the deprecated lspconfig.<server>.setup({...}) style.)
+      vim.lsp.config("lua_ls", { capabilities = capabilities })
+      vim.lsp.config("pyright", { capabilities = capabilities })
+      vim.lsp.config("ts_ls", { capabilities = capabilities })
+
+      vim.lsp.enable({ "lua_ls", "pyright", "ts_ls" })
+
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover docs" })
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+    end,
+  },
 })
